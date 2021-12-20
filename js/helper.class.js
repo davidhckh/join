1adb32c9f95d80b2fd770de5d93f169a19d4f378
David <david.heckhoff@gmail.com>
2021-12-21 00:15:52 +0100

    added helper clas

---
+++ js/helper.class.js
@@ -0,0 +1,25 @@
+export default class Helper {
+
+    allItems = []
+    test = 'hi'
+
+    constructor() {
+        this.downloadFromServer()
+    }
+
+    downloadFromServer() {
+        /**await.. */
+    }
+
+    uploadToServer() {
+        //backend.setItem
+    }
+
+    filterIDs () {
+
+    }
+
+    updateStatus(to) {
+
+    }  
+}
