287c6fb00745ffe0b5991b36de111b376b70c170
David <david.heckhoff@gmail.com>
2022-01-02 17:05:28 +0100

    made backlog items expandable

--- js/backlog.class.js
+++ js/backlog.class.js
@@ -11,8 +11,6 @@ class Backlog {
     renderContainer = document.getElementById('render-container');
 
     constructor() {
-        this.renderContainer.innerHTML = '';
-
         setURL('http://gruppe-142.developerakademie.net/smallest_backend_ever');
 
         this.helper = new Helper();
@@ -26,38 +24,80 @@ class Backlog {
     async setup() {
         await this.helper.getDataFromServer();
 
+        this.filterTasks();
         this.render();
     };
 
+    filterTasks() {
+        this.backlogTasks = this.helper.allTasks.filter((task) => task.state === 'backlog');
+    }
+
     render() {
         this.renderContainer.innerHTML = '';
-        this.helper.allTasks.forEach((task) => {
-            if(task.state === 'backlog') {
-                this.renderContainer.insertAdjacentHTML('beforeend',
-                `<div class="entry-container box row" onclick="backlog.addToBoard(${task.timeOfCreation})">
-                    <div class="urgency-color urgency-${task.urgency}"></div>
-                    <span class="responsive-header">Assigned to:</span>
-                    <div class="assigned-to-container row">
-                        <img class="assigned-to-image" src="${task.assignedTo.image}">
-                        <div class="column assigned-to-details">
-                            <span class="assigned-to-name">${task.assignedTo.name}</span>
-                            <span class="assigned-to-mail">${task.assignedTo.mail}</span>
+
+
+        this.backlogTasks.forEach((task) => {
+            this.renderContainer.insertAdjacentHTML('beforeend',
+                `<div class="entry-container box column" onclick="backlog.checkContainerVisibility(${this.backlogTasks.indexOf(task)})">
+                    <div class="row entry-top-container">
+                        <div class="urgency-color urgency-${task.urgency}"></div>
+                        <span class="responsive-header">Assigned to:</span>
+                        <div class="assigned-to-container row">
+                            <img class="assigned-to-image" src="${task.assignedTo.image}">
+                            <div class="column assigned-to-details">
+                                <span class="assigned-to-name">${task.assignedTo.name}</span>
+                                <span class="assigned-to-mail">${task.assignedTo.mail}</span>
+                            </div>
+                        </div>
+                        <span class="responsive-header">Category:</span>
+                        <div class="category-container">
+                            <span>${this.capitalizeFirstLetter(task.category)}</span>
+                        </div>
+                        <span class="responsive-header">Title:</span>
+                        <div class="details-container">
+                            <span>${this.capitalizeFirstLetter(task.title)}</span>
                         </div>
                     </div>
-                    <span class="responsive-header">Category:</span>
-                    <div class="category-container">
-                        <span>${this.capitalizeFirstLetter(task.category)}</span>
+
+                    <div id="expanded-${this.backlogTasks.indexOf(task)}" class="expanded-entry-container row hide">
+                        <div class="column expanded-category-container">
+                            <h5>Category:</h5>
+                            <span>Design</span>
+                        </div>
+                    <div class="column">
+                        <h5>Description:</h5>
+                        <span>Müll rausbringen, Mehl, Sahne</span>
                     </div>
-                    <span class="responsive-header">Title:</span>
-                    <div class="details-container">
-                        <span>${this.capitalizeFirstLetter(task.title)}</span>
+                    <div class="add-to-board-container" onclick="event.stopPropagation(); backlog.addToBoard(${task.timeOfCreation})">Add To Board</div>
                     </div>
-                    <div class="add-to-board-container">+</div>
                 </div>`
-            );
-            };
-        });
-    };
+            )
+        })
+    }
+
+    checkContainerVisibility(index) {
+        if(document.getElementById('expanded-' + index).classList.contains('hide')) {
+            this.expandContainer(index)
+        } else {
+            this.collapseContainer(index)
+        }
+    }
+
+    expandContainer(index) {
+        this.collapseAllContainers()
+
+        document.getElementById('expanded-' + index).classList.remove('hide')
+    }
+
+    collapseAllContainers() {
+        for(let i = 0; i < this.backlogTasks.length; i++) {
+            this.collapseContainer(i)
+        }
+    }
+
+    collapseContainer(index) {
+        document.getElementById('expanded-' + index).classList.add('hide')
+    }
 
     /**
      * Add Item to board and upload to server + render after
@@ -68,6 +108,7 @@ class Backlog {
 
         this.helper.allTasks[itemIndex].state = 'to-do';
 
+        this.filterTasks();
         this.render();
         this.helper.uploadToServer();
     };
